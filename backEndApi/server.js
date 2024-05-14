d57fc72159904d644fb7fce0d89e138576c99641
rahemur0910 <skrahemurrahaman@gmail.com>
2024-05-14 19:47:16 +0530

    all deleted

--- backEndApi/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-const PORT=process.env.PORT || 6060;
-
-const app = require("./app.js")
-
-app.listen(PORT,()=>{
-    console.log(`server is listening at http://localhost:${PORT}...`);
-});
