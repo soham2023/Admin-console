974b822b427a3ac4964f59e6e30936bacba498f3
rahemur0910 <skrahemurrahaman@gmail.com>
2024-05-18 13:41:43 +0530

    4ph2fixed

--- backEndApi/app.js
+++ backEndApi/app.js
@@ -3,11 +3,13 @@ const app = express();
 
 // Import routes
 const adminRoute = require('./routes/adminRoute');
+const productRoutes = require('./routes/productRoutes')
 
 // Middleware
 app.use(express.json());
 
 // Routes
 app.use('/api', adminRoute);
+app.use('/api', productRoutes);
 
 module.exports = app;
