ab5086717e9b3d0baa303ea4ce12c7fa2dc5b8a2
rahemur0910 <skrahemurrahaman@gmail.com>
2024-05-18 12:46:15 +0530

    4ph1bugFixed

--- backEndApi/config/dbconn.js
+++ backEndApi/config/dbconn.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 
 function connectToDatabase() {
-    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
 }
